edaad4b9947fd0a0a1dc846a4359e2b3efaef6ec
Rafał Szarowicz <93370514+rafalit@users.noreply.github.com>
2024-03-14 13:45:37 +0100

    HeadRotation, RestartingGame, quite done

--- Snake/MainWindow.xaml.cs
+++ Snake/MainWindow.xaml.cs
@@ -24,7 +24,17 @@ public partial class MainWindow : Window
         {GridValue.Food, Images.Food }
     };
 
-    private readonly int rows = 15, cols = 15;
+
+    private readonly Dictionary<Direction, int> dirToRotation = new()
+    {
+        {Direction.Up, 0},
+        {Direction.Right, 90},
+        {Direction.Down, 180},
+        {Direction.Left, 270}
+    };
+
+
+    private readonly int rows = 10, cols = 20;
     private readonly Image[,] gridImages;
     private GameState gameState;
     private bool gameRunning;
@@ -51,6 +61,8 @@ public partial class MainWindow : Window
         await ShowCountDown();
         Overlay.Visibility = Visibility.Hidden;
         await GameLoop();
+        await ShowGameOver();
+        gameState = new GameState(rows, cols);
     }
 
     private async void Window_PreviewKeyDown(object sender, KeyEventArgs e)
@@ -118,6 +130,9 @@ public partial class MainWindow : Window
     private Image[,] SetupGrid()
     {
         Image[,] images = new Image[rows, cols];
+        GameGrid.Rows = rows;
+        GameGrid.Columns = cols;
+        GameGrid.Width = GameGrid.Height * (cols / (double)rows);
 
 
         for (int r = 0; r < rows; r++)
@@ -126,7 +141,8 @@ public partial class MainWindow : Window
             {
                 Image imageControl = new Image
                 {
-                    Source = Images.Empty
+                    Source = Images.Empty,
+                    RenderTransformOrigin = new Point(0.5, 0.5)
                 };
 
                 images[r, c] = imageControl;
@@ -140,6 +156,7 @@ public partial class MainWindow : Window
     private void Draw()
     {
         DrawGrid();
+        DrawSnakeHead();
         ScoreText.Text = $"SCORE {gameState.Score}";
     }
 
@@ -151,7 +168,31 @@ public partial class MainWindow : Window
             {
                 GridValue gridVal = gameState.Grid[r, c];
                 gridImages[r, c].Source = gridValToImage[gridVal];
+                gridImages[r, c].RenderTransform = Transform.Identity;
             }
+        }
+    }
+
+    private void DrawSnakeHead()
+    {
+        Position headPos = gameState.HeadPosition();
+        Image image = gridImages[headPos.Row, headPos.Col];
+        image.Source = Images.Head;
+
+        int rotation = dirToRotation[gameState.Dir];
+        image.RenderTransform = new RotateTransform(rotation);
+    }
+
+    private async Task DrawDeadSnake()
+    {
+        List<Position> positions = new List<Position>(gameState.SnakePositions());
+
+        for(int i=0; i<positions.Count; i++)
+        {
+            Position pos = positions[i];
+            ImageSource source = (i == 0) ? Images.DeadHead : Images.DeadBody;
+            gridImages[pos.Row, pos.Col].Source = source;
+            await Task.Delay(50);
         }
     }
 
@@ -162,5 +203,13 @@ public partial class MainWindow : Window
             OverlayText.Text = i.ToString();
             await Task.Delay(500);
         }
+    }
+
+    private async Task ShowGameOver()
+    {
+        await DrawDeadSnake();
+        await Task.Delay(500);
+        Overlay.Visibility = Visibility.Visible;
+        OverlayText.Text = "Naciśnij przycisk aby znów zagrać!";
     }
 }
