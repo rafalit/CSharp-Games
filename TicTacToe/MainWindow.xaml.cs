39f883ec4be0bfccd40051c129e56fa97763d074
Rafał Szarowicz <93370514+rafalit@users.noreply.github.com>
2024-03-17 13:54:26 +0100

    visible board, gameplay quite done

--- TicTacToe/MainWindow.xaml.cs
+++ TicTacToe/MainWindow.xaml.cs
@@ -16,8 +16,66 @@ namespace TTT;
 /// </summary>
 public partial class MainWindow : Window
 {
+    private readonly Dictionary<Player, ImageSource> imageSources = new()
+    {
+        {Player.X, new BitmapImage(new Uri("pack://application:,,,/Assets/X15.png")) },
+        {Player.O, new BitmapImage(new Uri("pack://application:,,,/Assets/O15.png")) }
+    };
+
+    private readonly Image[,] imageControls = new Image[3, 3];
+    private readonly GameState gameState = new GameState();
+
     public MainWindow()
     {
         InitializeComponent();
+        SetupGameGrid();
+
+        gameState.MoveMade += OnMoveMade;
+        gameState.GameEnded += OnGameEnded;
+        gameState.GameRestarted += OnGameRestarted;
+    }
+
+    private void SetupGameGrid()
+    {
+        for(int r=0; r<3; r++)
+        {
+            for(int c=0; c<3; c++)
+            {
+                Image imageControl = new Image();
+                GameGrid.Children.Add(imageControl);
+                imageControls[r, c] = imageControl;
+            }
+        }
+    }
+
+    private void OnMoveMade(int r, int c)
+    {
+        Player player = gameState.GameGrid[r, c];
+        imageControls[r, c].Source = imageSources[player];
+        PlayerImage.Source = imageSources[gameState.CurrentPlayer];
+    }
+
+    public void OnGameEnded(GameResult gameResult)
+    {
+
+    }
+
+    private void OnGameRestarted()
+    {
+
+    }
+
+    private void GameGrid_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        double squareSize = GameGrid.Width / 3;
+        Point clickPosition = e.GetPosition(GameGrid);
+        int row = (int)(clickPosition.Y / squareSize);
+        int col = (int)(clickPosition.X / squareSize);
+        gameState.MakeMove(row, col);
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+
     }
 }
